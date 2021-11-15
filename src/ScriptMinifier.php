7a89ca1a62673f37acb6ec92cfc5970ea07cc741
sanjeevaasoori <sanjeev.a.rangaswamy@gmail.com>
2021-11-15 16:45:43 +0530

    Version 1.2
    Commit to version 1.2

--- src/ScriptMinifier.php
+++ src/ScriptMinifier.php
@@ -4,14 +4,16 @@ namespace Sanjeev\Custom;
 
 class ScriptMinifier
 {
-    public function __construct()
+    public function __construct(bool $errorReporting=false)
     {
+        $this->opts['errorReporting']= $errorReporting;
     }
-    protected mixed $input, $content = [], $locks = [], $output = "";
+    protected mixed $input, $content = [], $locks = [];
+    public mixed $output = "";
     protected mixed $opts = [
         "allowComments" => false,
         "inputIsFile" => false,
-        "errorReporting" => false,
+        "obfuscate" => false
     ], $noNewLineCharacters = [
         '(' => true,
         '-' => true,
@@ -24,23 +26,24 @@ class ScriptMinifier
         '`' => true
     ];
     protected ?int $length = 0, $inputPos = 0;
-    protected static function echo(mixed $args, bool $auto_exit = false)
+
+    protected static function echo(mixed $args, bool $auto_exit = false): void
     {
         \Sanjeev\Custom\ErrorReporting::echo($args, $auto_exit);
     }
-    public function addOpts(mixed $opts): void
+    public function addOpts(array|string $opt_or_key, $value = null): void
     {
-        $this->opts = array_merge($this->opts, $opts);
+        if (is_array($opt_or_key)) {
+            $this->opts = array_merge($this->opts, $opt_or_key);
+        } else {
+            $this->opts[$opt_or_key] = $value;
+        }
     }
-    public function addOpt(mixed $kn, mixed $kv): void
+    public function minify(string $source, mixed $opts = []): mixed
     {
-        $this->opts[$kn] = $kv;
-    }
-    public function minify(mixed $opts = [], string $fileSrc = "", string $inputStream = ""): mixed
-    {
-        $this->init(opts: $opts, fileSrc: $fileSrc, inputStream: $inputStream);
+        $this->init(opts: $opts, source: $source);
+        $this->processComments();
         $this->lock();
-        $this->filterInput();
         $this->process();
         $this->clean();
         $this->unlock();
@@ -49,48 +52,38 @@ class ScriptMinifier
 
     protected function fileReader($fileSrc): mixed
     {
-        $proceed = false;
-        if (file_exists($fileSrc)) {
-            if ($fp = fopen($fileSrc, 'r')) {
-                $proceed = true;
-                fclose($fp);
-            }
-        }
-        if ($proceed) {
+        try {
             return file_get_contents($fileSrc);
-        } else {
+        } catch (\Throwable $th) {
             static::echo(args: "Failed to read Data Index", auto_exit: true);
         }
     }
 
-    protected function init(mixed $opts = [], string $fileSrc = "", string $inputStream = ""): void
+    protected function init(string $source, ?array $opts = null): void
     {
-        $this->opts = array_merge($this->opts, $opts);
-        if ($this->opts['errorReporting']) {
+        if ($opts !== null) {
+            $this->opts = array_merge($this->opts, $opts);
+        }
+        if ($this->opts['errorReporting'] ?? false) {
             \Sanjeev\Custom\ErrorReporting::init();
         }
-        if ($this->opts['inputIsFile']) {
-            $this->input = $this->fileReader($fileSrc);
+        if ($this->opts['inputIsFile'] ?? false) {
+            $this->input = $this->fileReader($source);
         } else {
-            if (strlen($inputStream) > 0) {
-                $this->input = $inputStream;
+            if (strlen($source) > 0) {
+                $this->input = $source;
             } else {
                 static::echo(args: "Invalid Input Stream", auto_exit: true);
             }
         }
     }
-    protected function filterInput()
-    {
-        $this->input = str_replace(["\r\n", '/**/', "\r"], ["\n", "", "\n"], $this->input);
-        $this->input .= PHP_EOL;
-        $this->length = strlen($this->input);
-        $this->content[0] = "\n";
-        $this->content[1] = $this->getReal();
-    }
-
     protected function process(): void
     {
-        while ($this->content[0] !== false && !is_null($this->content[0]) && $this->content[0] !== '') {
+        $this->input = str_replace(["\r\n", "\r"], ["\n", "\n"], $this->input) . "\n";
+        $this->length = strlen($this->input);
+        $this->content[0] = "\n";
+        $this->content[1] = $this->getRealCharacter();
+        while (($t = $this->content[0] ?? false) !== false && $t !== null && $t !== '') {
             switch ($this->content[0]) {
                 case "\n":
                     if ($this->content[1] !== false && isset($this->noNewLineCharacters[$this->content[1]])) {
@@ -136,7 +129,7 @@ class ScriptMinifier
                             break;
                     }
             }
-            $this->content[1] = $this->getReal();
+            $this->content[1] = $this->getRealCharacter();
             if (($this->content[1] == '/' && strpos('(,=:[!&|?', $this->content[0]) !== false)) {
                 $this->saveRegex();
             }
@@ -197,45 +190,21 @@ class ScriptMinifier
 
             $this->output .= $this->content[0];
         }
-        $this->content[1] = $this->getReal();
+        $this->content[1] = $this->getRealCharacter();
     }
 
     protected function lock(): mixed
     {
         $lock = '"LOCK---' . crc32(time()) . '"';
         $matches = [];
-        preg_match('/([+-])(\s+)([+-])/S', $this->input, $matches);
+        preg_match('/([+-])(\s+)([+-])/s', $this->input, $matches);
         if (empty($matches)) {
             return $this->input;
         }
-
         $this->locks[$lock] = $matches[2];
-
         $this->input = preg_replace('/([+-])\s+([+-])/S', "$1{$lock}$2", $this->input);
-        /* -- */
-
         return $this->input;
     }
-
-    protected function getReal(): mixed
-    {
-        $index = $this->inputPos;
-        $char = $this->getRealCharacter();
-        if ($char !== '/') {
-            return $char;
-        }
-        $this->content[2] = $this->getRealCharacter();
-        if ($this->content[2] === '/') {
-            $this->processComments(index: $index, delim: '/');
-            return $this->getReal();
-        } elseif ($this->content[2] === '*') {
-            $this->processComments(index: $index, delim: '*');
-            return $this->getReal();
-        }
-
-        return $char;
-    }
-
     protected function getRealCharacter(): mixed
     {
 
@@ -288,49 +257,12 @@ class ScriptMinifier
         return $this->inputPos < $this->length ? $this->input[$this->inputPos] : false;
     }
 
-    protected function processComments(int $index, string $delim): void
-    {
-        if ($delim === '/') {
-            $thirdCommentString = $this->getInputPosChar();
-            $this->getNextCharacter(str: '\n');
-            unset($this->content[2]);
-            if ($thirdCommentString == '@') {
-                $endPoint = $this->inputPos - $index;
-                $this->content[2] = "\n" . substr($this->input, $index, $endPoint);
-            }
-        }
-        if ($delim === '*') {
-            $posChar = $this->getCurrentChar();
-            $commentCharacter = $this->getCurrentChar();
-            if ($this->getNextCharacter(str: '*/')) {
-                $this->getCurrentChar();
-                $this->getCurrentChar();
-                $character = $this->getCurrentChar();
-                if (($this->opts['allowComments'])) {
-                    if ($index > 0) {
-                        $this->content[0] = " ";
-                        $this->output .= $this->content[0];
-                        if ($this->input[($index - 1)] === "\n") {
-                            $this->output .= "\n";
-                        }
-                    }
-                    $endPoint = ($this->inputPos - 1) - $index;
-                    $this->output .= substr($this->input, $index, $endPoint);
-                    $this->content[2] = $character;
-                }
-            } else {
-                static::echo(args: ['Unclosed multiline comment at position: \n' . ($this->inputPos - 2), 'Chararcted Caught\n', $this->input[$this->inputPos - 2]], auto_exit: true);
-            }
-            $this->content[2] = $character;
-        }
-    }
     protected function clean(): void
     {
         unset($this->input);
         $this->length = 0;
         $this->inputPos = 0;
-        $this->content[0] = $this->content[1] = '';
-        unset($this->content[2]);
+        unset($this->content);
         unset($this->opts);
     }
 
@@ -343,5 +275,44 @@ class ScriptMinifier
             $this->output = str_replace($lock, $replacement, $this->output);
         }
         return $this->output;
+    }
+    protected function processComments(): void
+    {
+        if ($this->opts['allowComments'] ?? false) {
+            return;
+        }
+        /*
+        Pattern taken from: https://stackoverflow.com/questions/30573253/remove-all-real-javascript-comments-in-php
+        By: Casimir et Hippolyte [ https://stackoverflow.com/users/2255089/casimir-et-hippolyte ]
+        */
+        $pattern = <<<'EOD'
+~
+(?(DEFINE)
+    (?<squoted> ' [^'\n\\]*+ (?: \\. [^'\n\\]* )*+ ' )
+    (?<dquoted> " [^"\n\\]*+ (?: \\. [^"\n\\]* )*+ " )
+    (?<tquoted> ` [^`\\]*+ (?s: \\. [^`\\]*)*+ ` )
+    (?<quoted>  \g<squoted> | \g<dquoted> | \g<tquoted> )
+    
+    (?<scomment> // \N* )
+    (?<mcomment> /\* [^*]*+ (?: \*+ (?!/) [^*]* )*+ \*/ )
+    (?<comment> \g<scomment> | \g<mcomment> )
+    
+    (?<pattern> / [^\n/*] [^\n/\\]*+ (?>\\.[^\n/\\]*)* / [gimuy]* ) 
+)
+
+(?=[[(:,=/"'`])
+(?|
+    \g<quoted> (*SKIP)(*FAIL)
+    |
+    ( [[(:,=] \s* ) (*SKIP) (?: \g<comment> \s* )*+ ( \g<pattern> )
+    | 
+    ( \g<pattern> \s* ) (?: \g<comment> \s* )*+ 
+    ( \. \s* ) (?:\g<comment> \s* )*+ ([A-Za-z_]\w*)
+    |
+    \g<comment>
+)
+~x
+EOD;
+        $this->input = preg_replace($pattern, '$9${10}${11}', $this->input);
     }
 }
